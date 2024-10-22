31cbee15b8a45d28a318efa4e317d5ee07938330
Ana Cristina <72415738@continental.edu.pe>
2024-10-22 00:22:20 -0500

    Caso sin elementos en la lista con excepción NoSePuedeCalcular (verde)

--- src/logica/operaciones.py
+++ src/logica/operaciones.py
@@ -1,4 +1,7 @@
 class NoSePuedeCalcular(Exception):
     pass
 
-
+class Calculadora:
+    def media(self, elementos):
+        if not elementos:
+            raise NoSePuedeCalcular("No se puede calcular la media de una lista vacía")
