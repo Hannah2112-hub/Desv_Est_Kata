4f541d12c8a564ee7b4a03b2a27cd8ff5ebc2e77
Ana Cristina <72415738@continental.edu.pe>
2024-10-22 00:42:41 -0500

    Caso con varios elementos en la lista (verde) Cualquier validación que sea necesaria en otros métodos (como para la desviación estándar) se puede reutilizar sin duplicar código

--- src/logica/operaciones.py
+++ src/logica/operaciones.py
@@ -2,9 +2,14 @@ class NoSePuedeCalcular(Exception):
     pass
 
 class Calculadora:
-    def media(self, elementos):
+    def validar_elementos(self, elementos):
         if not elementos:
             raise NoSePuedeCalcular("No se puede calcular la media de una lista vacía")
+
+        for elemento in elementos:
+            if not isinstance(elemento, (int, float)):
+                raise TypeError("Todos los elementos deben ser numéricos")
+
+    def media(self, elementos):
+        self.validar_elementos(elementos)
         return sum(elementos) / len(elementos)
-
-
